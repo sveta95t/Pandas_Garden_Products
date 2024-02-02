32080dfb134245ce33e2cbedfcb5cf4b605cf9ba
Sofiia.Starokozheva <starokozheva.sofiya@gmail.com>
2024-02-02 22:00:54 +0200

    add block categories

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Categories from "./Components/Categories";
+
+function App() {
+  return (
+    <div>
+      <Categories />
+    </div>
+  );
+}
+
+export default App;
