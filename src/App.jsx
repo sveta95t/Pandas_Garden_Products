e32a07bf2a4ba2c7be43d56f8d1902edaa850a6c
Sveta95t <sveta.titova95@mail.ru>
2024-02-11 22:50:26 +0100

    Discount Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Categories from "./Components/Categories";
+import Discount from "./Components/Discount/Discount";
 import Contact from "./Components/Contact";
 
 function App() {
   return (
     <div>
       <Categories />
+      <Discount />
       <Contact />
     </div>
   );
