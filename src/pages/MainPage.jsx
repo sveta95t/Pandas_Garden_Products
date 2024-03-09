6fd93c4f2b1f9ad09448710afd50cfbaf54a5563
Sofiia.Starokozheva <starokozheva.sofiya@gmail.com>
2024-03-09 14:03:55 +0200

    update

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -2,14 +2,15 @@ import Categories from "../Components/Categories";
 import React from 'react'
 import Header from "../Components/Header";
 import Sale from "../Components/Sale";
+import Discount from "../Components/Discount/Discount";
 
 function MainPage() {
   return (
     <>
       <Header />
       <Categories />
+      <Discount />
       <Sale />
-
     </>
   )
 }
